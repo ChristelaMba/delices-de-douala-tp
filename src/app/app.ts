282e6f1fc7332a8c4a7f454aa143074266771169
Christela Mba <mbachristela@gmail.com>
2026-06-29 08:21:39 +0100

    feat(restaurant-card): Carte restaurant avec name, district, speciality, et currentRating

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StarRating } from "./components/star-rating/star-rating";
+import { RestaurantCard } from "./components/restaurant-card/restaurant-card";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StarRating],
+  imports: [RouterOutlet, StarRating, RestaurantCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('delices-de-douala-tp');
+
+  testRestaurantCard = {
+    id:1, name: 'Le Calao Doré', district: 'Akwa, Douche', speciality:'Ndolé', currentRating:3
+  };
 }
--- src/app/components/restaurant-card/restaurant-card.ts
+++ src/app/components/restaurant-card/restaurant-card.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Restaurant } from '../../models/restaurant';
+import { StarRating } from '../star-rating/star-rating';
 
 @Component({
   selector: 'app-restaurant-card',
-  imports: [],
+  imports: [StarRating],
   templateUrl: './restaurant-card.html',
   styleUrl: './restaurant-card.css',
 })
-export class RestaurantCard {}
+export class RestaurantCard {
+  restaurant = input.required<Restaurant>();
+  restaurantRated = output<{restaurantId: number, newRating: number}>();
+
+  onRatingChanged(newRating: number) {
+    this.restaurantRated.emit({restaurantId: this.restaurant().id, newRating: newRating});
+  }
+}
