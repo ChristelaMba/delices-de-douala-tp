4b6d26a2b8d7c923f438468dc9eaa9aa02be6623
Christela Mba <mbachristela@gmail.com>
2026-06-29 07:28:24 +0100

    feat(star-reating): composant étoiles avec hover, clic et output

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StarRating } from "./components/star-rating/star-rating";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StarRating],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/star-rating/star-rating.ts
+++ src/app/components/star-rating/star-rating.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal, computed} from '@angular/core';
 
 @Component({
   selector: 'app-star-rating',
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './star-rating.html',
   styleUrl: './star-rating.css',
 })
-export class StarRating {}
+export class StarRating {
+  currentRating = input<number>(0);
+  ratingChanged = output<number>();
+
+  hoverRating = signal<number>(0);
+  displayRating = computed(() => this.hoverRating() || this.currentRating()); //Permet de garder la note affichée meme apres le clic
+
+  onHover(star: number) {
+    this.hoverRating.set(star); //on remplace hoverRating par star
+  }
+
+  onLeave() {
+    this.hoverRating.set(0); //on remet hoverRating à 0 (plus de survol)
+  }
+
+  selectRating(star: number) {
+    this.ratingChanged.emit(star); //On emet le output ratingChanged avec la valeur star
+  }
+}
