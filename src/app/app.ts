1bd9a04e141891aecefbe6d6bbfb40a0058141ad
Christela Mba <mbachristela@gmail.com>
2026-06-29 21:48:15 +0100

    feat(restaurant-list): grille des 6 restaurants avec @for et layout grid

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,17 +2,37 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StarRating } from "./components/star-rating/star-rating";
 import { RestaurantCard } from "./components/restaurant-card/restaurant-card";
+import { RestaurantList } from './components/restaurant-list/restaurant-list';
+
+interface Restaurant {
+  id: number;
+  name: string;
+  district: string;
+  speciality: string;
+  currentRating: number;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StarRating, RestaurantCard],
+  imports: [RouterOutlet, StarRating, RestaurantCard, RestaurantList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('delices-de-douala-tp');
 
-  testRestaurantCard = {
-    id:1, name: 'Le Calao Doré', district: 'Akwa, Douche', speciality:'Ndolé', currentRating:3
-  };
+  restaurants = signal<Restaurant[]>([ 
+  { id: 1, name: 'Le Calao Doré', district: 'Akwa', 
+    speciality: 'Ndolé aux crevettes', currentRating: 0 }, 
+  { id: 2, name: 'Chez Madame Ngono', district: 'Bonapriso', 
+    speciality: 'Eru aux pieds de bœuf', currentRating: 0 }, 
+  { id: 3, name: 'La Fourchette Camerounaise', district: 'Bonanjo', 
+    speciality: 'Poulet DG', currentRating: 0 }, 
+  { id: 4, name: 'Saveurs du Wouri', district: 'Bonamoussadi', 
+    speciality: 'Poisson braisé', currentRating: 0 }, 
+  { id: 5, name: "L'Akwa Gourmand", district: 'Akwa', 
+    speciality: 'Bobolo et sauce arachide', currentRating: 0 }, 
+  { id: 6, name: 'Le Royal de Bali', district: 'Bali', 
+    speciality: 'Koki et plantain', currentRating: 0 } 
+]); 
 }
--- src/app/components/restaurant-list/restaurant-list.ts
+++ src/app/components/restaurant-list/restaurant-list.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Restaurant } from '../../models/restaurant';
+import { RestaurantCard } from '../restaurant-card/restaurant-card';
 
 @Component({
   selector: 'app-restaurant-list',
-  imports: [],
+  imports: [RestaurantCard],
   templateUrl: './restaurant-list.html',
   styleUrl: './restaurant-list.css',
 })
-export class RestaurantList {}
+export class RestaurantList {
+  restaurants = input.required<Restaurant[]>();
+  restaurantRated = output<{restaurantId: number, newRating: number}>();
+
+  onRestaurantRated(event: {restaurantId:number, newRating:number}) {
+    this.restaurantRated.emit(event);
+  }
+}
